23514f69fb0e80f54d10932fdf65cf9b4a1c27e3
natalia-simonova <102836542+natalia-simonova@users.noreply.github.com>
2023-02-28 17:57:30 +1100

    Add files via upload

--- StageInfo.cs
+++ StageInfo.cs
@@ -12,118 +12,139 @@ using KSP.Sim.DeltaV;
 using KSP.Sim.impl;
 using UnityEngine;
 using SpaceWarp.API.Mods;
+using SpaceWarp.API.AssetBundles;
 
 namespace StageInfo {
 
     [MainMod]
     public class StageInfo : Mod {
 
-        static bool loaded = false;
+        private GUISkin _spaceWarpUISkin;
         private bool showGUI = false;
         private int windowWidth = 300;
         private Rect guiRect;
         private int windowHeight = 700;
         private GUIStyle horizontalDivider = new GUIStyle();
         private bool refreshing = false;
-        private bool subscribed = false;
+        // private bool subscribed = false;
 
         private Dictionary<int, SituationData> situData = new Dictionary<int, SituationData>();
         private Dictionary<int, bool> inAtmo = new Dictionary<int, bool>();
         private bool inVAB = false;
+        private bool hideExtra = false;
 
         public override void OnInitialized() {
-            Logger.Info("StageInfo Loaded");
-            if (loaded) Destroy(this);
-            loaded = true;
+            ResourceManager.TryGetAsset($"space_warp/swconsoleui/swconsoleUI/spacewarpConsole.guiskin", out _spaceWarpUISkin);
+            // SubscribeToMessages();
+            GameManager.Instance.Game.Messages.Subscribe<VesselDeltaVCalculationMessage>(GetSituationData);
+            GameManager.Instance.Game.Messages.Subscribe<GameStateChangedMessage>(UpdateGameState);
+            GameManager.Instance.Game.Messages.Subscribe<OpenEngineersReportWindowMessage>(OpenEngineersReport);
+            GameManager.Instance.Game.Messages.Subscribe<CloseEngineersReportWindowMessage>(CloseEngineersReport);
         }
 
-        void Awake() => guiRect = new Rect((Screen.width * 0.7f) - (windowWidth / 2), (Screen.height / 2) - (windowHeight / 2), 0, 0);
-        
-        private void Update() {
-            if (!subscribed && ShouldSubscribeToMessages()) SubscribeToMessages();
-            if (Input.GetKey(KeyCode.LeftAlt) && Input.GetKeyDown(KeyCode.E)) showGUI = !showGUI;
+        public void Awake() => guiRect = new Rect((Screen.width * 0.8632f) - (windowWidth / 2), (Screen.height / 2) - (windowHeight / 2), 0, 0);
+
+
+        //private void Update() {
+        //    if (!subscribed && ShouldSubscribeToMessages()) SubscribeToMessages();
+        //    if (Input.GetKey(KeyCode.LeftAlt) && Input.GetKeyDown(KeyCode.E)) showGUI = !showGUI;
+        //}
+
+        //private bool ShouldSubscribeToMessages() {
+        //    if (subscribed) return false;
+        //    if (GameManager.Instance == null) return false;
+        //    if (GameManager.Instance.Game == null) return false;
+        //    if (GameManager.Instance.Game.Messages == null) return false;
+        //    return true;
+        //}
+
+        private void UpdateGameState(MessageCenterMessage msg) {
+            inVAB = GameManager.Instance.Game.GlobalGameState.GetState() == GameState.VehicleAssemblyBuilder;
+            if (!inVAB && GameManager.Instance.Game.GlobalGameState.GetState() != GameState.FlightView) showGUI = false;
         }
 
-        private bool ShouldSubscribeToMessages() {
-            if (subscribed) return false;
-            if (GameManager.Instance == null) return false;
-            if (GameManager.Instance.Game == null) return false;
-            if (GameManager.Instance.Game.Messages == null) return false;
-            return true;
-        }
+        private void OpenEngineersReport(MessageCenterMessage msg) => showGUI = true;
+        private void CloseEngineersReport(MessageCenterMessage msg) => showGUI = false;
 
-        private void UpdateGameState(MessageCenterMessage msg) => inVAB = GameManager.Instance.Game.GlobalGameState.GetState() == GameState.VehicleAssemblyBuilder;
-        
         void OnGUI() {
             if (!showGUI) return;
+            GUI.skin = _spaceWarpUISkin;
             guiRect = GUILayout.Window(
                 GUIUtility.GetControlID(FocusType.Passive),
                 guiRect,
                 FillGUI,
-                "Stage Info",
+                "<color=#696DFF>// STAGE INFO</color>",
                 GUILayout.Height(0),
                 GUILayout.Width(windowWidth));
         }
 
         private void FillGUI(int windowID) {
-            if (refreshing || situData.Count == 0) {
-                GUILayout.Label("Calculating Stage Info...");
-            } else {
+            if (refreshing || situData.Count == 0) GUILayout.Label("<i>Calculating Stage Info...</i>");
+            else {
                 int cols = inVAB ? 4 : 3;
-                GUILayout.BeginHorizontal();
-                GUILayout.Label("Stage", GUILayout.Width(windowWidth / cols));
-                GUILayout.Label("TWR", GUILayout.Width(windowWidth / cols));
-                GUILayout.Label("∆v", GUILayout.Width(windowWidth / cols));
-                if (inVAB) GUILayout.Label("Situation", GUILayout.Width(windowWidth / 4));
-                // GUILayout.Label("Thrust", GUILayout.Width(windowWidth / 6));
-                // GUILayout.Label("ISP", GUILayout.Width(windowWidth / 6));
-                GUILayout.EndHorizontal();
                 horizontalDivider.fixedHeight = 2;
                 horizontalDivider.margin = new RectOffset(0, 0, 4, 4);
                 GUILayout.Box("", horizontalDivider);
+                GUILayout.BeginHorizontal();
+                GUILayout.Label("<b> <color=#C0C7D5>S.</color> <color=#0A0B0E>|</color></b>", GUILayout.Width(windowWidth / 6));
+                GUILayout.Label("<b><color=#C0C7D5>T/W</color>  <color=#0A0B0E>|</color></b>", GUILayout.Width(windowWidth / 6));
+                GUILayout.Label(" <b><color=#C0C7D5>∆<i>v</i></color></b>", GUILayout.Width(windowWidth / 4));
+                GUILayout.Label($"<b>{(inVAB ? "<color=#C0C7D5>SITUATION" : " <color=#0A0B0E>|</color> <color=#C0C7D5>BURN")}</color></b>", GUILayout.Width(windowWidth / 4));
+                GUILayout.EndHorizontal();
+                
+                GUILayout.Box("", horizontalDivider);
                 for (int i = 0; i < situData.Count; i++) {
+                    if (inVAB && hideExtra && situData[i].dV[DeltaVSituationOptions.Altitude] == 0) continue;
+                    if (!inVAB && hideExtra && i < situData.Count - 1) continue;
                     DeltaVSituationOptions situ = inVAB ? (inAtmo[i] ? DeltaVSituationOptions.SeaLevel : DeltaVSituationOptions.Vaccum) : DeltaVSituationOptions.Altitude;
                     GUILayout.BeginHorizontal();
-                    GUILayout.Label($"{situData.Count - i:00}", GUILayout.Width(windowWidth / cols));
-                    GUILayout.Label($"{situData[i].twr[situ]:N2}", GUILayout.Width(windowWidth / cols));
-                    GUILayout.Label($"{situData[i].dV[situ]:N0} m/s", GUILayout.Width(windowWidth / cols));
-                    if (inVAB) inAtmo[i] = GUILayout.Toggle(inAtmo[i], "Sea Level", GUILayout.Width(windowWidth / 4));
-                    // GUILayout.Label($"{situData[i].thrust[DeltaVSituationOptions.Altitude]:N1} kN", GUILayout.Width(windowWidth / 6));
-                    // GUILayout.Label($"{situData[i].isp[DeltaVSituationOptions.Altitude]:N0} s", GUILayout.Width(windowWidth / 6));
-                    GUILayout.EndHorizontal();
-                }
-            }
-
+                    GUILayout.Label($" <color=#C0C7D5>{situData.Count - i:00}</color> <color=#0A0B0E><b>|</b></color>", GUILayout.Width(windowWidth / 6));
+                    float twr = situData[i].twr[situ];
+                    GUILayout.Label($"<color={(twr < 1 ? "#E04949" : (twr > 1.25 ? "#0DBE2A" : "#E0A400"))}>{twr:N2}</color> <color=#0A0B0E><b>|</b></color>", GUILayout.Width(windowWidth / 6));
+                    GUILayout.Label($"<color=#C0C7D5>{situData[i].dV[situ]:N0} m/s</color>", GUILayout.Width(windowWidth / 4));
+                    if (inVAB) inAtmo[i] = GUILayout.Toggle(inAtmo[i], $"<color=#C0C7D5> {(inAtmo[i] ? "1 atm" : "Vac.")}</color>", GUILayout.Width(windowWidth / 4));
+                    else GUILayout.Label($" <color=#0A0B0E><b>|</b></color> <color=#C0C7D5>{situData[i].burn:N0} s</color>", GUILayout.Width(windowWidth / 4));
+                    GUILayout.EndHorizontal(); }
+                GUILayout.BeginHorizontal();
+                hideExtra = GUILayout.Toggle(hideExtra, $"<color=#C0C7D5>{(inVAB ? "Hide Empty" : "Hide Future")}</color>");
+                GUILayout.FlexibleSpace();
+                showGUI = !GUILayout.Button("<color=#C0C7D5>Close</color>", GUILayout.Width(windowWidth / 6));
+                GUILayout.EndHorizontal(); }
             GUI.DragWindow(new Rect(0, 0, 10000, 500));
         }
 
-        private void PopulateReadouts(MessageCenterMessage msg) {
-            Debug.Log("[StageInfo]: Received DeltaV message");
+        private void GetSituationData(MessageCenterMessage msg) {
+            if (inVAB) Debug.Log("[StageInfo]: Received DeltaV message");
             VesselDeltaVCalculationMessage dvm = msg as VesselDeltaVCalculationMessage;
             if (dvm == null) return;
             refreshing = true;
             situData.Clear();
             inAtmo.Clear();
             foreach (DeltaVStageInfo stageInfo in dvm.DeltaVComponent.StageInfo) {
+                
                 situData.Add(stageInfo.Stage, new SituationData(stageInfo));
                 if (inVAB) inAtmo.Add(stageInfo.Stage, stageInfo.Stage == dvm.DeltaVComponent.StageInfo.Count - 1); }
             refreshing = false;
         }
 
-        private void SubscribeToMessages() {
-            GameManager.Instance.Game.Messages.Subscribe<VesselDeltaVCalculationMessage>(PopulateReadouts);
-            GameManager.Instance.Game.Messages.Subscribe<GameStateChangedMessage>(UpdateGameState);
-            subscribed = true;
-        }
+        //private void SubscribeToMessages() {
+        //    GameManager.Instance.Game.Messages.Subscribe<VesselDeltaVCalculationMessage>(PopulateReadouts);
+        //    GameManager.Instance.Game.Messages.Subscribe<GameStateChangedMessage>(UpdateGameState);
+        //    GameManager.Instance.Game.Messages.Subscribe<OpenEngineersReportWindowMessage>(OpenEngineersReport);
+        //    GameManager.Instance.Game.Messages.Subscribe<CloseEngineersReportWindowMessage>(CloseEngineersReport);
+        //    // subscribed = true;
+        //}
 
         public class SituationData {
 
+            public double burn;
             public Dictionary<DeltaVSituationOptions, float> twr;
             public Dictionary<DeltaVSituationOptions, double> dV;
             // public Dictionary<DeltaVSituationOptions, float> thrust;
             // public Dictionary<DeltaVSituationOptions, double> isp;
 
             public SituationData(DeltaVStageInfo stageInfo) {
+                burn = stageInfo.StageBurnTime;
                 twr = new Dictionary<DeltaVSituationOptions, float>() {
                     [DeltaVSituationOptions.SeaLevel] = stageInfo.GetSituationTWR(DeltaVSituationOptions.SeaLevel),
                     [DeltaVSituationOptions.Altitude] = stageInfo.GetSituationTWR(DeltaVSituationOptions.Altitude),
